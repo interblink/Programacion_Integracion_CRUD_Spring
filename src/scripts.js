e33795f981ecd50148c330264c121f822a53db44
interblink <jleonardo5086@outlook.com>
2024-06-10 11:01:55 -0500

    Spring-Boot

--- src/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let columns = document.querySelectorAll('.col-md-6');
-    columns.forEach(function (col) {
-        col.addEventListener('click', function () {
-            col.style.backgroundColor = col.style.backgroundColor === 'red' ? '#FF0000' : 'red';
-        });
-    });
-});
